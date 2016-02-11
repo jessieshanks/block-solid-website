678caa12d7e0f3c9f0fb70d827841a41c3e2386a
Jessie Shanks <jessie.shanks@gmail.com>
2016-02-10 22:38:51 -0600

    emails contact form info

--- routes/contact-form-submissions.js
+++ routes/contact-form-submissions.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var ContactFormSubmission = require('../models/contact-form-submission');
+var sendgrid  = require('sendgrid')('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');
+
 
 
 /* GET contact form page */
@@ -10,11 +12,10 @@ router.get('/', function(req, res, next) {
 
 
 
-
-/* Post contact form information to database */
-
+/* POST contact form info */
 router.post('/', function(req, res) {
 
+  // Post contact form information to database
   var contactFormSubmission = new ContactFormSubmission ({
     name: req.body.name,
     email: req.body.email,
@@ -29,6 +30,22 @@ router.post('/', function(req, res) {
     }
     res.status(200).json(contactFormSubmission);
   });
+
+
+  // Send email with contact form information
+  var email = new sendgrid.Email({
+    to: 'info@block-solid.com',
+    fromname: req.body.name,
+    from: req.body.email,
+    subject: req.body.subject,
+    text: req.body.message
+  });
+
+  sendgrid.send(email, function(err, json) {
+      if (err) { return console.error(err); }
+      console.log(json);
+  });
+
 });
 
 
